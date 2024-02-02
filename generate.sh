d1d91021ac2245fc556dcf9fff1deac7ecf4b3dd
izrik <izrik@izrik.com>
2024-02-01 22:37:13 -0600

    Print pdf directly from python.

---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ./main.py > output.html && xhtml2pdf output.html output.html.pdf
+./main.py --pdf > output.pdf
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
+import argparse
+import os
+import sys
 
 from jinja2 import Template
+from xhtml2pdf import pisa
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--format', choices=('html', 'pdf'),
+                        default='html')
+    parser.add_argument('--html', action='store_const',
+                        const='html', dest='format')
+    parser.add_argument('--pdf', action='store_const',
+                        const='pdf', dest='format')
+
+    args = parser.parse_args()
+
     with open('template.html') as f:
         template = Template(f.read())
         items = [
@@ -11,7 +25,14 @@ def main():
             'Check the mail',
             'Buy stamps',
         ]
-    print(template.render(items=items))
+    html = template.render(items=items)
+    if args.format == 'html':
+        print(html)
+    elif args.format == 'pdf':
+        with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
+            pisa.CreatePDF(html, dest=stdout)
+    else:
+        raise Exception(f'Unknown format "{args.format}"')
 
 
 if __name__ == '__main__':
