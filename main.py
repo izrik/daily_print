5788d5b7e38c4204b91bc3157b4330c49238ab9f
izrik <izrik@izrik.com>
2024-02-02 10:31:29 -0600

    Add 'dot items' which don't need to be checked off.

--- main.py
+++ main.py
@@ -16,17 +16,28 @@ def main():
     parser.add_argument('--pdf', action='store_const',
                         const='pdf', dest='format')
     parser.add_argument('--name')
-    parser.add_argument('items', nargs='+')
+    parser.add_argument('items', nargs='*')
+    parser.add_argument('--dot', '-d', action='append', nargs='+')
+    parser.add_argument('--check', '-c', action='append', nargs='+')
 
     args = parser.parse_args()
 
     with open('template.html') as f:
         template = Template(f.read())
-    items = args.items
+    items = []
+    if args.items:
+        items.extend(args.items)
+    if args.check:
+        for list_ in args.check:
+            items.extend(list_)
+    dot_items = []
+    if args.dot:
+        for list_ in args.dot:
+            dot_items.extend(list_)
     heading = 'Tasks for today'
     if args.name:
         heading = f'{args.name}\'s tasks for today'
-    html = template.render(items=items, heading=heading)
+    html = template.render(items=items, heading=heading, dot_items=dot_items)
     if args.format == 'html':
         print(html)
     elif args.format == 'pdf':
